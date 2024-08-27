72247e165ebdbb1d293a55909c1fc67c07fdc716
Vojtech Benes <vojtech.benes@carvago.com>
2024-08-27 14:22:27 +0200

    initial commit

---
+++ src/components/heading.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Heading = (children: React.ReactNode) => {
+	return(
+		<>
+			{children}
+		</>
+	)
+}
---
+++ src/utils/texts.ts
@@ -0,0 +1,7 @@
+export const texts = {
+	club: {
+		title: 'TJ Sokol Svinov',
+		subtitle: 'Oddíl národní házené',
+		description: 'Oddíl TJ Sokol Svinov má více než 90.letou tradici v českém sportu Národní házená. Máme za sebou řadu úspěchů jak v soutěžích dospělých, tak v žákovských kategoriích. Snažíme se o propagaci našeho milovaného, ryze českého sportu mezi veřejností a hlavně dodat dětem, mladistvými a dospělým, sportovní průpravu v dnešní sedavé době.'
+	}
+}
