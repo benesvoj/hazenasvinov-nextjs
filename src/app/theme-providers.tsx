7e84606145294489de388b396378a2d7f46794c6
Vojtech Benes <vojtech.benes@carvago.com>
2025-08-07 08:52:27 +0200

    Revert "commit latest changes from cursor, totaly broken design"
    This reverts commit c7b1035b97b75eff0cbd1ebd7c6e80535813a71e.

--- src/app/theme-providers.tsx
+++ src/app/theme-providers.tsx
@@ -9,7 +9,7 @@ export function ThemeProviders({ children }: { children: React.ReactNode }) {
 	return (
 		<ThemeProvider attribute="class" defaultTheme={siteMetadata.theme} enableSystem>
 			<HeroUIProvider>
-				{children}
+			{children}
 			</HeroUIProvider>
 		</ThemeProvider>
 	)
