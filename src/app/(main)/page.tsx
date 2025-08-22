2e1fc61d471dece9026414b051207dd17f9292da
Vojtech Benes <vojtech.benes@carvago.com>
2025-08-22 20:02:49 +0200

    remove unused components

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,15 +1,12 @@
 'use client';
 
 import MatchSchedule from "@/components/MatchSchedule";
-import { HeroSection, HeroSectionV3, CallTheActionSection, SponsorsSection, ClubHighlightSection, PostSection} from "./components";
+import { CallTheActionSection, SponsorsSection, ClubHighlightSection, PostSection} from "./components";
 
 export default function Page() {
 
   return (
     <div className="space-y-8">
-      {/* Hero Section */}
-      {/* <HeroSection /> */}
-      {/* <HeroSectionV3 /> */}
       <PostSection />
 
       {/* Match Schedule & Results */}
