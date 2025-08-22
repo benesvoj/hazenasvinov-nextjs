0303f8982a019f9cf3036ced4697fdc23e70d817
Vojtech Benes <vojtech.benes@carvago.com>
2025-08-22 23:08:22 +0200

    fixes

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -8,20 +8,9 @@ export default function Page() {
   return (
     <div className="space-y-8">
       <PostSection />
-
-      {/* Match Schedule & Results */}
       <MatchSchedule />
-
-      {/* Latest News - Modern Design */}
-      {/* <PostSection /> */}
-
-      {/* Club Highlights */}
       <ClubHighlightSection />
-
-      {/* Sponsors Section */}
       <SponsorsSection />
-
-      {/* Call to Action */}
       <CallTheActionSection />
     </div>
   );
